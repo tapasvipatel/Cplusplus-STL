0788dcf4ab56f0014f17091d834074b2ffa9a4cd
Tapasvi <tapasvi101@gmail.com>
2020-09-01 21:20:15 -0400

    Added vector classes functionality.

---
+++ Sequence Containers/vector.cpp
@@ -0,0 +1,17 @@
+#include "vector.h"
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+vector::vector()
+{
+	cout << "HELLO EVERYONE" << endl;
+}
+
+int main()
+{
+	vector();
+
+	return 0;
+}
---
+++ Sequence Containers/vector.hpp
@@ -0,0 +1,13 @@
+#ifndef __VECTOR_H__
+#define __VECTOR_H__
+
+class vector
+{
+private:
+
+
+public:
+	vector();
+};
+
+#endif //__VECTOR_H__
