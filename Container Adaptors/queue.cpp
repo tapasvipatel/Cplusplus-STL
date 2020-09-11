c228ed557129a33d612f4f6d38f40e05f3b4aa22
Tapasvi <tapasvi101@gmail.com>
2020-09-10 20:55:05 -0400

    Added class declaration and source files for Queue container.

---
+++ Container Adaptors/queue.cpp
@@ -0,0 +1,8 @@
+#include "queue.hpp"
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+	return 0;
+}
---
+++ Container Adaptors/queue.hpp
@@ -0,0 +1,10 @@
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+
+template <typename T>
+class Queue
+{
+
+};
+
+#endif // __QUEUE_H__
